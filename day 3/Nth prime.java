5e708fd9defbaa54c5b96d68df85ce83ae0577d3
Rajalakshmi1405 <90809044+Rajalakshmi1405@users.noreply.github.com>
2023-02-09 11:06:50 +0530

    Update Nth prime.java

--- day 3/Nth prime.java
+++ day 3/Nth prime.java
@@ -2,16 +2,21 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
- public int isPrime(int input1){
-  int count=0;
- for(int i=2;i<=Math.sqrt(input1);i++)
-{
- if(input1%i==0)
+ public int NthPrime(int input1){
+       int count=0,pcount=0,i;
+  for(i=2;i<=100000;i++)
+  {
+   count=0;
+   for(int j=2;j<=Math.sqrt(i);j++)
+   {
+    if(i%j==0)
      count++;
-}
-if(count==0)
- return 2;
- else
-return 1;
- }
-}
+   }
+   if(count==0)
+    pcount++;
+   if(pcount==input1)
+    break;
+  }
+  return i; 
+}}
+
